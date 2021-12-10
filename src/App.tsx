c9f843bd2e173dad7e9c5a394f3cb03750846406
Blackraven93 <reblackraven@gmail.com>
2021-12-10 20:33:41 +0900

    Feat: Link to use react-dom about coin component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${props => props.theme.bgColor};
     color:${props => props.theme.textColor};
   }
+
+  a {
+    color:inherit;
+  }
 `
 
 
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,56 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
 
 interface RouteParmas {
     coinId: string;
 }
 
+interface RouterState {
+    name?:string;
+    rank?:number;
+}
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin:0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    color:${props => props.theme.accentColor};
+    font-size: 48px;
+`
+
+const Loader = styled.span`
+    display: block;
+    text-align: center;
+`;
+
 const Coin = () => {
-    const { coinId } = useParams() as RouteParmas;
-    console.log(coinId)
-    return <h1>Coin: {coinId}</h1>
+
+    const { coinId } = useParams() as RouteParmas; // 이건 coinId을 (Param) 받음
+    const [loading, setLoading] = useState(true);
+    const { state } = useLocation(); // Link state로 내려준 값을 받을 수 있다.
+    const name = state?.name || "loading..." as RouterState;
+    const rank = state?.rank || 0 as RouterState;
+    // 문제는 버튼을 눌러야 Location 정보가 넘어간다.
+    return (
+        <Container>
+            <Header>
+                <Title>{name}</Title>
+            </Header>
+            { loading ? (<Loader>Loading...</Loader>) : null}
+        </Container>
+    )
 }
 
 export default Coin;
